f04d22112f1d5f4d076f76264ec6e59610f0f99d
dt2576 <scarlest2576@gmail.com>
2026-06-24 14:38:04 +0700

    chore: create base backend

---
+++ code/backend/src/test/java/com/local/Lowlands/LowlandsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.local.Lowlands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowlandsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
